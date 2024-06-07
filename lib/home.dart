9393293d0d2e6b84a90ee1a99a598a1e2e168bb1
sahillathwal <sahillathwal566@gmail.com>
2024-06-07 14:32:29 +0530

    adding state feedback v1

--- lib/home.dart
+++ lib/home.dart
@@ -8,29 +8,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> _pageData = [];
+  List<String>? _pageData;
+  bool get _fetchingData => _pageData == null;
+
   @override
   void initState() {
+    _getListData(hasData: false)
+        .then((data) => setState(() {
+              if (data.isEmpty) {
+                data.add("No data available");
+                // _pageData = ['No data available'];
+              }
+              _pageData = data;
+            }))
+        .catchError((error) => setState(() {
+              _pageData = [error];
+            }));
     super.initState();
-    _getListData().then((data) => setState(() {
-          _pageData = data;
-        }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
-      body: ListView.builder(
-        itemCount: _pageData.length,
-        itemBuilder: (context, index) => _getListItemUi(index),
-      ),
+      body: _fetchingData
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _pageData?.length,
+              itemBuilder: (context, index) => _getListItemUi(index),
+            ),
     );
   }
 
 // Return a list of data after 1 second to emulate network request
-  Future<List<String>> _getListData() async {
-    await Future.delayed(const Duration(seconds: 1));
+  Future<List<String>> _getListData(
+      {bool hasError = false, hasData = true}) async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (hasError) {
+      return Future.error('Error fetching data');
+    }
+
+    if (!hasData) return [];
+
     return List<String>.generate(10, (index) => '$index title');
   }
 
@@ -42,7 +64,7 @@ class _HomeState extends State<Home> {
           borderRadius: BorderRadius.circular(5.0), color: Colors.grey[600]),
       child: Center(
         child: Text(
-          _pageData[index],
+          _pageData![index],
           style: const TextStyle(
             color: Colors.white,
           ),
