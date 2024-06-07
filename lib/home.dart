342afa6d326459a23906052a4b22e00ea829cf5a
sahillathwal <sahillathwal566@gmail.com>
2024-06-07 13:44:24 +0530

    initial android setup

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> _pageData = [];
+  @override
+  void initState() {
+    super.initState();
+    _getListData().then((data) => setState(() {
+          _pageData = data;
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: Colors.grey[900],
+      body: ListView.builder(
+        itemCount: _pageData.length,
+        itemBuilder: (context, index) => _getListItemUi(index),
+      ),
+    );
+  }
+
+// Return a list of data after 1 second to emulate network request
+  Future<List<String>> _getListData() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return List<String>.generate(10, (index) => '$index title');
+  }
+
+  Widget _getListItemUi(int index) {
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      height: 50.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0), color: Colors.grey[600]),
+      child: Center(
+        child: Text(
+          _pageData[index],
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import './home.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Home());
   }
 }
